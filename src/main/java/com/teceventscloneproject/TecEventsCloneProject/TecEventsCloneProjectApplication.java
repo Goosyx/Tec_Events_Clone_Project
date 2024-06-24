fd7903995bb762a3755b222e4315c70de41540bd
Sergio Murilo <sergiomurilo-cardoso@hotmail.com>
2024-06-24 17:52:28 -0300

    feat: first commit

---
+++ src/main/java/com/teceventscloneproject/TecEventsCloneProject/TecEventsCloneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.teceventscloneproject.TecEventsCloneProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TecEventsCloneProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TecEventsCloneProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teceventscloneproject/TecEventsCloneProject/TecEventsCloneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teceventscloneproject.TecEventsCloneProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TecEventsCloneProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
