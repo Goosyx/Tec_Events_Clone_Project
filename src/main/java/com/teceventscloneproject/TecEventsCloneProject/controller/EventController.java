95329f34323eee446dc06c5f57a0edc2165b11dd
Sergio Murilo <sergiomurilo-cardoso@hotmail.com>
2024-07-03 19:43:03 -0300

    feat: creates a list of registered events

--- src/main/java/com/teceventscloneproject/TecEventsCloneProject/controller/EventController.java
+++ src/main/java/com/teceventscloneproject/TecEventsCloneProject/controller/EventController.java
@@ -2,11 +2,14 @@ package com.teceventscloneproject.TecEventsCloneProject.controller;
 
 import com.teceventscloneproject.TecEventsCloneProject.domain.event.Event;
 import com.teceventscloneproject.TecEventsCloneProject.domain.event.EventRequestDTO;
+import com.teceventscloneproject.TecEventsCloneProject.domain.event.EventResponseDTO;
 import com.teceventscloneproject.TecEventsCloneProject.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -29,4 +32,13 @@ public class EventController {
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
     }
+
+@GetMapping
+public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+    List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page,size);
+    return ResponseEntity.ok(allEvents);
+
+
+}
+
 }
--- src/main/java/com/teceventscloneproject/TecEventsCloneProject/service/EventService.java
+++ src/main/java/com/teceventscloneproject/TecEventsCloneProject/service/EventService.java
@@ -3,18 +3,26 @@ package com.teceventscloneproject.TecEventsCloneProject.service;
 import com.amazonaws.services.s3.AmazonS3;
 import com.teceventscloneproject.TecEventsCloneProject.domain.event.Event;
 import com.teceventscloneproject.TecEventsCloneProject.domain.event.EventRequestDTO;
+import com.teceventscloneproject.TecEventsCloneProject.domain.event.EventResponseDTO;
 import com.teceventscloneproject.TecEventsCloneProject.repositories.EventRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
+
+import static java.util.Arrays.stream;
 
 @Service
 public class EventService {
@@ -48,6 +56,28 @@ public class EventService {
 
         return newEvent;
     }
+
+    public List<EventResponseDTO> getUpcomingEvents(int page, int size){
+        Pageable pageable = PageRequest.of(page, size);
+        Page<Event> eventsPage = this.repository.findUpcomingEvents(new Date(), pageable);
+        return eventsPage.map(event -> new EventResponseDTO(event.getId(), event.getTitle(), event.getDescription(), event.getDate(), "", "", event.getRemote(), event.getEventUrl(), event.getImgUrl()))
+        .stream().toList();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
     private String uploadImg(MultipartFile multipartFile){
         String filename = UUID.randomUUID() + "-" + multipartFile.getOriginalFilename();
